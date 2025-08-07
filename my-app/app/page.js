1f99c50216a6323e785ce60626e392b6c759f2e8
Ragini-Tiwari <tiwariragini1054@gmail.com>
2025-08-07 23:04:12 +0530

    first step of project is done

---
+++ my-app/app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      AIspire – (AI + Aspire) — Inspiring through AI
+      <br/>
+      <Button>Click</Button>
+    </div>
+  );
+
+}
