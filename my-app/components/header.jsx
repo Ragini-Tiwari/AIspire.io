022399a5fbb0351e9d86b2e55b5158b45a795c29
Ragini-Tiwari <tiwariragini1054@gmail.com>
2025-08-13 23:59:12 +0530

    Add navbar and some user authentication function

--- my-app/components/header.jsx
+++ my-app/components/header.jsx
@@ -1,16 +1,29 @@
 import { SignedIn, SignedOut, UserButton, SignUpButton } from "@clerk/nextjs";
+import Image from "next/image";
+import link from "next/link";
 
 const Header = () => {
   return (
-    <>
+    <header>
+      <nav>
+        <link rel="stylesheet" href="" />
+        <img
+          src="/logo.png"
+          alt="AIspire logo"
+          width={200}
+          height={60}
+          className="h-12 py-1 w-auto object-contain"
+        />
+      </nav>
+
       <SignedOut>
         <SignUpButton />
       </SignedOut>
-      
+
       <SignedIn>
         <UserButton />
       </SignedIn>
-    </>
+    </header>
   );
 };
 
