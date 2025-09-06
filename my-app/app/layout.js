dc344f97e303e04052625b9b05584874f38eec4c
Ragini-Tiwari <tiwariragini1054@gmail.com>
2025-09-06 19:45:58 +0530

    update some changes

--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-import Header from "@/components/Header";   // ✅ Match file name casing
+import Header from "@/components/Header";   // ✅ Match file case 
 import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
